03c6d70df1c9ca807a5c21cb5caba18048d1b7e3
Zeyu Chen <xinyifly@live.cn>
2018-07-25 20:20:09 +0800

    feat(index): import admin-lte

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'font-awesome/css/font-awesome.min.css'
+import 'admin-lte'
+import 'admin-lte/dist/css/AdminLTE.min.css'
+import 'admin-lte/dist/css/skins/_all-skins.min.css'
